c654d56eb01199f355cadf0dce79522bbc7cfa9d
quanggacademy <quangg.academy@gmail.com>
2026-07-19 19:00:29 +0700

    Add files via upload

---
+++ JS.Vscode/JS.js
@@ -0,0 +1,14 @@
+function upDate(previewPic) {
+	document.getElementById('image').innerHTML = previewPic.alt;
+
+    document.getElementById('image').style.backgroundImage = "url('" + previewPic.src + "')";
+}
+
+function unDo() {
+
+	var originalText = "Hover over an image below to display here.";
+
+	document.getElementById('image').innerHTML = originalText;
+
+	document.getElementById('image').style.backgroundImage = "url('')";
+}
